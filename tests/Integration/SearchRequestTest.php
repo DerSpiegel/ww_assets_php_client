911f1e862322b8e26cafec78e02b42d679ea868c
Tim Strehle <tim.strehle@spiegel.de>
2023-10-16 13:36:56 +0200

    Add SearchRequestTest::testMalformedQuery()

--- tests/Integration/SearchRequestTest.php
+++ tests/Integration/SearchRequestTest.php
@@ -2,16 +2,25 @@
 
 namespace DerSpiegel\WoodWingAssetsClientTests\Integration;
 
+use DerSpiegel\WoodWingAssetsClient\Exception\BadRequestAssetsException;
 use DerSpiegel\WoodWingAssetsClient\Service\SearchRequest;
 use DerSpiegel\WoodWingAssetsClientTests\Fixtures\IntegrationFixture;
 
 
 final class SearchRequestTest extends IntegrationFixture
 {
-    public function test(): void
+    public function testEmptySearch(): void
     {
         $response = (new SearchRequest($this->assetsClient, q: '', num: 0))();
 
         $this->assertGreaterThan(0, $response->totalHits);
+    }
+
+
+    public function testMalformedQuery(): void
+    {
+        $this->expectException(BadRequestAssetsException::class);
+
+        $response = (new SearchRequest($this->assetsClient, q: 'noSuchField:', num: 0))();
     }
 }
